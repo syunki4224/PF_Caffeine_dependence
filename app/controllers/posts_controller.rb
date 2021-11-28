b1799b7fd57e95ca44c4ffed31f382ce19600a08
syunki4224 <syunkikoike4224@gmail.com>
2021-11-28 20:38:20 +0000

    post_edit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -30,12 +30,23 @@ class PostsController < ApplicationController
   end
 
   def edit
+    @post = Post.find(params[:id])
+
   end
 
   def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      render "edit"
+    end
   end
 
   def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
   end
 
   private
