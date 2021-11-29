68daa6d781390fb62820827954c6026f377122b9
syunki4224 <syunkikoike4224@gmail.com>
2021-11-29 02:56:22 +0000

    post_comment_fin

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,7 +1,29 @@
 class PostCommentsController < ApplicationController
+  before_action :authenticate_user!
+
   def create
+    @post = Post.find(params[:post_id])
+    @post_comment = PostComment.new(post_comment_params)
+    @post_comment.post_id = @post.id
+    @post_comment.user_id = current_user.id
+    if @post_comment.save
+      redirect_to post_path(@post.id)
+    else
+      redirect_to request.referer
+    end
   end
 
   def destroy
+    @post = Post.find(params[:post_id])
+    post_comment = @post.post_comments.find(params[:id])
+    post_comment.destroy
+    redirect_to request.referer
+  end
+
+
+  private
+
+  def post_comment_params
+   params.require(:post_comment).permit(:comment)
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,8 @@ class Post < ApplicationRecord
     has_many :post_comments, dependent: :destroy
     has_many :favorites, dependent: :destroy
     attachment :post_image, destroy:false
-
+    
+    
     def favorited_by?(user)
       favorites.where(user_id: user.id).exists?
     end
